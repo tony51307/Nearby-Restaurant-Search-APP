faa5d7c75a64a7a1d6ec3acc0118ad39ca9a5192
tony51307 <59693085+tony51307@users.noreply.github.com>
2021-05-23 21:48:40 +0800

    Update AppDelegate.swift

--- Restaurant_Search_Machine/AppDelegate.swift
+++ Restaurant_Search_Machine/AppDelegate.swift
@@ -17,8 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSPlacesClient.provideAPIKey("AIzaSyCnFSAheEhXIrj2QrRuFHtM54gAWgRDlR0")
-        GMSServices.provideAPIKey("AIzaSyCnFSAheEhXIrj2QrRuFHtM54gAWgRDlR0")
+        GMSPlacesClient.provideAPIKey("") //enter the key for Google Map API
+        GMSServices.provideAPIKey("") //enter the key for Google Map API
         
 
         return true
